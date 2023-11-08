a9bf89f57c778e134cd5cbc646b9602b62f9bd58
Tuğçe Dokgöz <tugcedokgoz201@gmail.com>
2023-11-08 10:06:43 +0300

    created entities

---
+++ Core/Onion.Domain/Entities/Detail.cs
@@ -0,0 +1,27 @@
+﻿using Onion.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onion.Domain.Entities
+{
+	public class Detail : EntityBase
+	{
+        public Detail()
+        {
+            
+        }
+        public Detail(string title, string description,int categoryId)
+        {
+            CatgeoryId = categoryId;
+			Title = title;
+			Description = description;
+		}
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int CatgeoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
