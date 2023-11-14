68cc5b6f3416af68c249e4f15477274ea01a02fa
Tuğçe Dokgöz <tugcedokgoz201@gmail.com>
2023-11-14 09:19:33 +0300

    added IWriteRepository,WriteRepository

--- Core/Onion.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/Onion.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Onion.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Onion.Application.Interfaces.Repositories
 {
-	public interface IWriteRepository
+	public interface IWriteRepository<T> where T : class, IEntitiyBase, new()
 	{
+		Task AddAsync(T entity);
+		Task AddRangeAsync(IList<T> entities);
+		Task<T> UpdateAsync(T entity);
+		Task HardDeleteAsync(T entity);
+
+
 	}
 }
--- Infrastructure/Onion.Persistence/Registration.cs
+++ Infrastructure/Onion.Persistence/Registration.cs
@@ -19,6 +19,7 @@ namespace Onion.Persistence
 			services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 			services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+			services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 		}
 	}
 }
